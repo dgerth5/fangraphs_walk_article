4a61ca5ecbdb8cef6cffe3844442b4f71e78148a
dgerth5 <33240415+dgerth5@users.noreply.github.com>
2021-06-25 21:40:58 -0400

    Update fangraphs bb study.R

--- fangraphs bb study.R
+++ fangraphs bb study.R
@@ -1,17 +1,11 @@
 library(tidyverse)
-library(stargazer)
 library(car)
-library(olsrr)
 
-data = read.csv("INPUT DATASET HERE")
+data = read.csv("INSERT DATA HERE")
 data = data[-10] # delete weird NA column
 colnames(data)[1] = "last_name"
 data[data$player_id %in% names(which(table(data$player_id) == 1)), ] # remove players that only appeared once
 head(data)
-
-data = data %>% # calculate bb/9
-  mutate(bb9 = p_walk / p_formatted_ip * 9,
-         adj_year = year - 2016) 
 
 data = data[order(data$player_id, -data$year),] # sort by player id and descending year
 head(data) 
@@ -34,13 +28,16 @@ data = na.omit(data)
 
 data = data %>%
   mutate(bb_binary = if_else(p_bb_percent < 8, 1 , 0 ),
-         prior_binary = if_else(prior_bb_per < 7.5, 1, 0)) 
+         prior_binary = if_else(prior_bb_per < 7.5, 1, 0),
+         bb_combined = prior_bb_per + p_bb_percentage) 
 
-m0 = lm(change_bb_per ~ change_fb_velo + prior_bb_per + player_age, data = data)
+m0 = lm(change_bb_per ~ change_fb_velo + bb_combined + player_age, data = data)
 summary(m0)
+vif(m0)
 
 data1 = data %>%
   filter(prior_binary == 1)
 
-m1 = lm(change_bb_per ~ change_fb_velo + prior_bb_per + player_age, data = data1)
+m1 = lm(change_bb_per ~ change_fb_velo + bb_combined + player_age, data = data1)
 summary(m1)
+vif(m1)
